df1492277f4d8b2abcaf3aef007b6fc1d63ae9e9
mtalhas <mtalha.dev@gmail.com>
2017-04-28 01:22:00 +0500

    tensorflow version

---
+++ transfer_style.py
@@ -0,0 +1,11 @@
+import time
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import PIL.Image
+import cv2
+
+
+print("==================Style Transfer Program==================")
+
+print("tensorflow_version:",tf.__version__)
